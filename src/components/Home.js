156aaa36a0ebbe5bdc479d93fc01443062218745
vijaySai-22 <vijaysaiprasadpujari@gmail.com>
2022-07-15 12:34:31 +0530

    fixed: icons not show bcoz w3schools kitcode used

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,8 @@ export default function Home() {
                 My name is <u>VIJAY</u>.
               </h1>
               <p>Front-end web developer</p>
-              <Button variant="dark">Download CV</Button>
-              {"  "}
-              <Button variant="outline-dark">Mail</Button>
+              <Button variant="dark" className="mr-5 d-btn" ><i class="fas fa-download"></i>{' '}Download CV</Button>
+              <Button variant="outline-dark" ><i class="fas fa-briefcase"></i>{' '}See My Work</Button>
             </Col>
             {/* <Col xs={12} md={6}>
                 
